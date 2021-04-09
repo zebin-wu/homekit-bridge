8c7f1d46dd0b87fcfa3e12649d364347adfb1f26
Zebin Wu <zebin.wu@foxmail.com>
2021-04-09 17:10:58 +0800

    Implement lhaplib.

--- cmake/bridge.cmake
+++ cmake/bridge.cmake
@@ -2,7 +2,9 @@
 set(BRIDGE_DIR "${TOP_DIR}/bridge")
 set(BRIDGE_SRC_DIR "${BRIDGE_DIR}/src")
 set(BRIDGE_INC_DIR "${BRIDGE_DIR}/include")
+set(BRIDGE_LUA_DIR "${BRIDGE_DIR}/lua")
 
 # collect bridge sources
 set(BRIDGE_SRCS "${BRIDGE_SRC_DIR}/App.c"
+                "${BRIDGE_SRC_DIR}/lhaplib.c"
                 "${BRIDGE_SRC_DIR}/DB.c")
