2d7557ddd15c1b990e033c9949fe017a228da234
KNpTrue <zebin.wu@foxmail.com>
2021-04-13 00:09:09 +0800

    Fix comment errors.

--- cmake/platform.cmake
+++ cmake/platform.cmake
@@ -3,5 +3,5 @@ set(PLATFORM_DIR "${TOP_DIR}/platform")
 set(PLATFORM_INC_DIR "${PLATFORM_DIR}/include")
 set(PLATFORM_SRC_DIR "${PLATFORM_DIR}/src")
 
-# collect bridge sources
+# collect platform sources
 set(PLATFORM_SRCS "${PLATFORM_SRC_DIR}/lpfmlib.c")
