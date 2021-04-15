54de5b1f9b79cac4405ae76c29d0bd215603a750
KNpTrue <zebin.wu@foxmail.com>
2021-04-15 23:04:23 +0800

    Move lpfmlib to bridge module.

--- cmake/platform.cmake
+++ cmake/platform.cmake
@@ -1,7 +1,3 @@
 # directory
 set(PLATFORM_DIR "${TOP_DIR}/platform")
 set(PLATFORM_INC_DIR "${PLATFORM_DIR}/include")
-set(PLATFORM_SRC_DIR "${PLATFORM_DIR}/src")
-
-# collect platform sources
-set(PLATFORM_SRCS "${PLATFORM_SRC_DIR}/lpfmlib.c")
