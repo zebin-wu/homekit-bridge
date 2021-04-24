864c7574b8a1d7c78114a59c910cb30084e83ead
KNpTrue <zebin.wu@foxmail.com>
2021-04-24 23:33:20 +0800

    Add console to platform ESP.

---
+++ platform/esp/main/app_console.h
@@ -0,0 +1,20 @@
+// Copyright (c) 2021 KNpTrue and homekit-bridge contributors
+//
+// Licensed under the MIT License.
+// You may not use this file except in compliance with the License.
+// See [CONTRIBUTORS.md] for the list of homekit-bridge project authors.
+
+#ifndef APP_CONSOLE_H
+#define APP_CONSOLE_H
+
+/**
+ * Initialize console.
+ */
+void app_console_init(void);
+
+/**
+ * Start the console.
+ */
+void app_console_start(void);
+
+#endif /* APP_CONSOLE_H */
