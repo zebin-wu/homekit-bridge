88d18aa74ecabd77f04b4f4bbe83a64530fcbc29
Zebin Wu <zebin.wu@foxmail.com>
2021-05-06 20:47:01 +0800

    Remove extra library rbtree.

--- cmake/datastruct.cmake
+++
@@ -1,7 +0,0 @@
-# directory
-set(RBTREE_DIR "${TOP_DIR}/ext/rbtree")
-set(DATASTRUCT_INC_DIR "${RBTREE_DIR}")
-
-# collect datastruct sources
-set(RBTREE_SRCS "${RBTREE_DIR}/rbtree.c")
-set(DATASTRUCT_SRCS "${RBTREE_SRCS}")
