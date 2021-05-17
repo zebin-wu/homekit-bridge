7e35d5b870988f54169f10f8d31a8f7af750f78b
KNpTrue <zebin.wu@foxmail.com>
2021-05-17 22:14:20 +0800

    Add a util function to split a string.

--- bridge/scripts/util.lua
+++ bridge/scripts/util.lua
@@ -20,4 +20,16 @@ function util.searchKey(t, v)
     return nil
 end
 
+---Split a string.
+---@param s string
+---@param c string
+---@return table results
+function util.split(s, c)
+    local rl = {}
+    for w in s:gmatch("[^" .. c .. "]+") do
+        table.insert(rl, w)
+    end
+    return rl
+end
+
 return util
