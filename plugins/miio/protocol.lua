8bc092e7118e98cd178bf5e81e25918ef33c06fd
KNpTrue <zebin.wu@foxmail.com>
2021-07-07 23:45:11 +0800

    Implement unpack in miio.protocol

--- plugins/miio/protocol.lua
+++ plugins/miio/protocol.lua
@@ -7,7 +7,7 @@ local logger = log.getLogger("miio.protocol")
 ProtocolPriv = {}
 
 ---
---- Packet format
+--- Message format.
 ---
 --- 0                   1                   2                   3
 --- 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
@@ -57,14 +57,21 @@ ProtocolPriv = {}
 ---      encrypted with AES-128: see below.
 ---      length = packet_length - 0x20
 ---
+---@class ProtocolMessage
+---
+---@field unknown integer
+---@field did integer
+---@field stamp integer
+---@field checksum string
+---@field data string
 
----Pack a packet.
+---Pack a message to a binary package.
 ---@param unknown integer Unknown: 32-bit.
 ---@param did integer Device ID: 32-bit.
 ---@param stamp integer Stamp: 32 bit unsigned int.
 ---@param checksum string MD5 checksnum: 128-bit.
 ---@param data? string Optional variable-sized data.
----@return string packet
+---@return string package
 function protocol.pack(unknown, did, stamp, checksum, data)
     assert(#checksum == 16)
     local len = 32
@@ -76,7 +83,29 @@ function protocol.pack(unknown, did, stamp, checksum, data)
     return string.pack(fmt, 0x2131, len, unknown, did, stamp, checksum, data)
 end
 
----Pack a hello packet.
+---Unpack a message from a binary package.
+---@param package string A binary package.
+---@return ProtocolMessage|nil message
+function protocol.unpack(package)
+    if string.unpack(">I2", package, 1) ~= 0x2131 then
+        return nil
+    end
+
+    local len = string.unpack(">I2", package, 3)
+    if len < 32 or len ~= #package then
+        return nil
+    end
+
+    return {
+        unknown = string.unpack(">I4", package, 5),
+        did = string.unpack(">I4", package, 9),
+        stamp = string.unpack(">I4", package, 13),
+        checksum = string.unpack("c16", package, 17),
+        data = string.unpack("c" .. len - 32, package, 33)
+    }
+end
+
+---Pack a hello package.
 ---@return string packet
 function protocol.packHello()
     return protocol.pack(
@@ -88,7 +117,7 @@ function protocol.packHello()
 end
 
 ---Scan for devices in the network.
----@param cb fun(device: table) Function call when the device is scaned.
+---@param cb fun(addr: string, devId: integer) Function call when the device is scaned.
 ---@param timeout integer Timeout period (in seconds).
 ---@param addr? string Target Address.
 ---@return boolean status true on success, false on failure.
@@ -109,7 +138,12 @@ function protocol.scan(cb, timeout, addr)
         ProtocolPriv.scanTimer = nil
     end)
     handle:sendto(protocol.packHello(), addr or "255.255.255.255", 54321)
-    handle:setRecvCb(function (data, from_addr, from_port) end)
+    handle:setRecvCb(function (data, from_addr, from_port, cb)
+        local m = protocol.unpack(data)
+        if m and m.unknown == 0 and m.data == nil then
+            cb(from_addr, m.did)
+        end
+    end, cb)
     ProtocolPriv.scanHandle = handle
     return true
 end
