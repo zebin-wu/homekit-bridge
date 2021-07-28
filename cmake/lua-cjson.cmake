b934e326d362b14a54ae0801457d1e62649363a4
Zebin Wu <zebin.wu@foxmail.com>
2021-07-28 23:50:39 +0800

    Add ext lua cjson library.

---
+++ cmake/lua-cjson.cmake
@@ -0,0 +1,11 @@
+# directory
+set(LUA_CJSON_DIR ${TOP_DIR}/ext/lua-cjson)
+set(LUA_CJSON_INC_DIR ${LUA_CJSON_DIR})
+set(LUA_CJSON_SRC_DIR ${LUA_CJSON_DIR})
+
+# collect lua-cjson sources
+set(LUA_CJSON_SRCS
+    ${LUA_CJSON_SRC_DIR}/lua_cjson.c
+    ${LUA_CJSON_SRC_DIR}/strbuf.c
+    ${LUA_CJSON_SRC_DIR}/fpconv.c
+)
