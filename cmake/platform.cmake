e7d9ae5782037680087d732e8c3a6be1109c7eb0
Zebin Wu <zebin.wu@foxmail.com>
2021-08-13 21:05:22 +0800

    Move common UDP implementation to platform POSIX

--- cmake/platform.cmake
+++ cmake/platform.cmake
@@ -3,6 +3,8 @@ set(PLATFORM_DIR ${TOP_DIR}/platform)
 set(PLATFORM_INC_DIR ${PLATFORM_DIR}/include)
 set(PLATFORM_COMMON_DIR ${PLATFORM_DIR}/common)
 set(PLATFORM_COMMON_SRC_DIR ${PLATFORM_COMMON_DIR}/src)
+set(PLATFORM_COMMON_POSIX_DIR ${PLATFORM_DIR}/posix)
+set(PLATFORM_COMMON_POSIX_SRC_DIR ${PLATFORM_COMMON_POSIX_DIR}/src)
 set(PLATFORM_MBEDTLS_DIR ${PLATFORM_DIR}/mbedtls)
 set(PLATFORM_MBEDTLS_SRC_DIR ${PLATFORM_MBEDTLS_DIR}/src)
 set(PLATFORM_OPENSSL_DIR ${PLATFORM_DIR}/openssl)
@@ -12,10 +14,20 @@ set(PLATFORM_LINUX_SRC_DIR ${PLATFORM_LINUX_DIR}/src)
 set(PLATFORM_ESP_DIR ${PLATFORM_DIR}/esp/components/platform)
 set(PLATFORM_ESP_SRC_DIR ${PLATFORM_ESP_DIR}/src)
 
+# collect platform headers
+set(PLATFORM_HEADERS
+    ${PLATFORM_INC_DIR}/pal/board.h
+    ${PLATFORM_INC_DIR}/pal/memory.h
+    ${PLATFORM_INC_DIR}/pal/hap.h
+    ${PLATFORM_INC_DIR}/pal/net/types.h
+    ${PLATFORM_INC_DIR}/pal/net/udp.h
+    ${PLATFORM_INC_DIR}/pal/cipher.h
+)
+
 # collect platform Linux sources
 set(PLATFORM_LINUX_SRCS
     ${PLATFORM_COMMON_SRC_DIR}/hap.c
-    ${PLATFORM_COMMON_SRC_DIR}/net/udp.c
+    ${PLATFORM_COMMON_POSIX_SRC_DIR}/net/udp.c
     ${PLATFORM_OPENSSL_SRC_DIR}/cipher.c
     ${PLATFORM_LINUX_SRC_DIR}/board.c
     ${PLATFORM_LINUX_SRC_DIR}/memory.c
@@ -25,18 +37,8 @@ set(PLATFORM_LINUX_SRCS
 # collect platform ESP sources
 set(PLATFORM_ESP_SRCS
     ${PLATFORM_COMMON_SRC_DIR}/hap.c
-    ${PLATFORM_COMMON_SRC_DIR}/net/udp.c
+    ${PLATFORM_COMMON_POSIX_SRC_DIR}/net/udp.c
     ${PLATFORM_MBEDTLS_SRC_DIR}/cipher.c
     ${PLATFORM_ESP_SRC_DIR}/board.c
     ${PLATFORM_ESP_SRC_DIR}/memory.c
-)
-
-# collect platform headers
-set(PLATFORM_HEADERS
-    ${PLATFORM_INC_DIR}/pal/board.h
-    ${PLATFORM_INC_DIR}/pal/memory.h
-    ${PLATFORM_INC_DIR}/pal/hap.h
-    ${PLATFORM_INC_DIR}/pal/net/types.h
-    ${PLATFORM_INC_DIR}/pal/net/udp.h
-    ${PLATFORM_INC_DIR}/pal/cipher.h
 )
