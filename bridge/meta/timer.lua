6057c433f59febbdedc1dca12e69861a05a59a59
Zebin Wu <zebin.wu@foxmail.com>
2021-08-20 22:56:32 +0800

    Add timer.start()

--- bridge/meta/timer.lua
+++ bridge/meta/timer.lua
@@ -5,11 +5,14 @@ local timer = {}
 local _timer = {}
 
 ---Create a timer.
+---@param cb fun(arg?: any) Function to call when the timer expires.
+---@param arg? any Argument passed to the callback.
+---@return _timer _timer
+function timer.create(cb, arg) end
+
+---Start the timer.
 ---@param ms integer Monotonic trigger time in milliseconds.
----@param cb fun(...) Function to call when the timer expires.
----@vararg any Arguments passed to the callback.
----@return _timer|nil _timer
-function timer.create(ms, cb, ...) end
+function _timer:start(ms) end
 
 ---Cancel the timer before trigger.
 function _timer:cancel() end
