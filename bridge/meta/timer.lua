ea8ed8d144d55c0113ae8ae9e52072ec7a52c4ab
Zebin Wu <zebin.wu@foxmail.com>
2021-08-24 20:58:06 +0800

    Set timeout callback in timer.start()

--- bridge/meta/timer.lua
+++ bridge/meta/timer.lua
@@ -4,15 +4,15 @@ local timer = {}
 ---@class _timer:userdata Timer object.
 local _timer = {}
 
----Create a timer.
----@param cb fun(arg?: any) Function to call when the timer expires.
----@param arg? any Argument passed to the callback.
+---Create a timer object.
 ---@return _timer _timer
-function timer.create(cb, arg) end
+function timer.create() end
 
 ---Start the timer.
 ---@param ms integer Monotonic trigger time in milliseconds.
-function _timer:start(ms) end
+---@param cb fun(arg?: any) Function to call when the timer expires.
+---@param arg? any Argument passed to the callback.
+function _timer:start(ms, cb, arg) end
 
 ---Cancel the timer before trigger.
 function _timer:cancel() end
