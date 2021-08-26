9ceecbb49ddac9ac3c5faa754e47309ca7e88377
Zebin Wu <zebin.wu@foxmail.com>
2021-08-27 00:26:24 +0800

    Remove --filter="runtime/casting".

--- cmake/extension.cmake
+++ cmake/extension.cmake
@@ -104,7 +104,7 @@ function(check_style target top_dir)
             COMMENT "Check ${rel_path}"
             COMMAND ${CPPLINT}
                 --linelength=120
-                --filter=-readability/casting,-build/include,-runtime/casting,-runtime/arrays
+                --filter=-readability/casting,-build/include,-runtime/arrays
                 ${rel_path}
             COMMAND touch ${output}
             WORKING_DIRECTORY ${top_dir}
