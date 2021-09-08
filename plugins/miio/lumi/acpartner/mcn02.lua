ca13916492090ff356f2ff66126ed977d7fe1fe4
KNpTrue <zebin.wu@foxmail.com>
2021-09-08 23:45:03 +0800

    Optimize the memory useage of lumi.acpartner.mcn02

--- plugins/miio/lumi/acpartner/mcn02.lua
+++ plugins/miio/lumi/acpartner/mcn02.lua
@@ -9,8 +9,6 @@ local HeatThrholdTemp = require "hap.char.HeatingThresholdTemperature"
 local SwingMode = require "hap.char.SwingMode"
 
 local acpartner = {}
-local model = "lumi.acpartner.mcn02"
-local logger = log.getLogger(model)
 
 ---Create a acpartner.
 ---@param device MiioDevice Device object.
@@ -18,10 +16,12 @@ local logger = log.getLogger(model)
 ---@param conf MiioDeviceConf Device configuration.
 ---@return HapAccessory accessory HomeKit Accessory.
 function acpartner.gen(device, info, conf)
-    local iids = {}
+    local iids = {
+        acc = hap.getNewBridgedAccessoryID()
+    }
 
     for i, v in ipairs({
-        "acc", "serv", "active", "curTemp", "curState", "tgtState", "coolThrTemp", "heatThrTemp", "swingMode"
+        "serv", "active", "curTemp", "curState", "tgtState", "coolThrTemp", "heatThrTemp", "swingMode"
     }) do
         iids[v] = hap.getNewInstanceID()
     end
@@ -48,7 +48,7 @@ function acpartner.gen(device, info, conf)
         category = "BridgedAccessory",
         name = conf.name or "Acpartner",
         mfg = "lumi",
-        model = model,
+        model = info.model,
         sn = info.mac,
         fwVer = info.fw_ver,
         hwVer = info.hw_ver,
@@ -68,33 +68,33 @@ function acpartner.gen(device, info, conf)
                     }
                 },
                 chars = {
-                    Active.new(iids.active, function (request, device)
+                    Active.new(iids.active, function (request, self)
                         local value
-                        if device:getProp("power") == "on" then
+                        if self:getProp("power") == "on" then
                             value = Active.value.Active
                         else
                             value = Active.value.Inactive
                         end
-                        logger:info("Read active: " .. util.searchKey(Active.value, value))
+                        self.logger:info("Read active: " .. util.searchKey(Active.value, value))
                         return value, hap.Error.None
-                    end, function (request, value, device)
-                        logger:info("Write active: " .. util.searchKey(Active.value, value))
+                    end, function (request, value, self)
+                        self.logger:info("Write active: " .. util.searchKey(Active.value, value))
                         local power
                         if value == Active.value.Active then
                             power = "on"
                         else
                             power = "off"
                         end
-                        device:setProp("power", power)
+                        self:setProp("power", power)
                         return hap.Error.None
                     end),
-                    CurTemp.new(iids.curTemp, function (request, device)
-                        local value = device:getProp("tar_temp")
-                        logger:info("Read CurrentTemperature: " .. value)
+                    CurTemp.new(iids.curTemp, function (request, self)
+                        local value = self:getProp("tar_temp")
+                        self.logger:info("Read CurrentTemperature: " .. value)
                         return value, hap.Error.None
                     end),
-                    CurHeatCoolState.new(iids.curState, function (request, device)
-                        local mode = device:getProp("mode")
+                    CurHeatCoolState.new(iids.curState, function (request, self)
+                        local mode = self:getProp("mode")
                         local value
                         if mode == "cool" then
                             value = CurHeatCoolState.value.Cooling
@@ -103,11 +103,11 @@ function acpartner.gen(device, info, conf)
                         else
                             value = CurHeatCoolState.value.Idle
                         end
-                        logger:info("Read CurrentHeaterCoolerState: " .. util.searchKey(CurHeatCoolState.value, value))
+                        self.logger:info("Read CurrentHeaterCoolerState: " .. util.searchKey(CurHeatCoolState.value, value))
                         return value, hap.Error.None
                     end),
-                    TgtHeatCoolState.new(iids.tgtState, function (request, device)
-                        local mode = device:getProp("mode")
+                    TgtHeatCoolState.new(iids.tgtState, function (request, self)
+                        local mode = self:getProp("mode")
                         local value
                         if mode == "cool" then
                             value = TgtHeatCoolState.value.Cool
@@ -116,10 +116,10 @@ function acpartner.gen(device, info, conf)
                         elseif mode == "auto" then
                             value = TgtHeatCoolState.value.HeatOrCool
                         end
-                        logger:info("Read TargetHeaterCoolerState: " .. util.searchKey(TgtHeatCoolState.value, value))
+                        self.logger:info("Read TargetHeaterCoolerState: " .. util.searchKey(TgtHeatCoolState.value, value))
                         return value, hap.Error.None
-                    end, function (request, value, device)
-                        logger:info("Write TargetHeaterCoolerState: " .. util.searchKey(TgtHeatCoolState.value, value))
+                    end, function (request, value, self)
+                        self.logger:info("Write TargetHeaterCoolerState: " .. util.searchKey(TgtHeatCoolState.value, value))
                         local mode
                         if value == TgtHeatCoolState.value.Cool then
                             mode = "cool"
@@ -128,53 +128,53 @@ function acpartner.gen(device, info, conf)
                         elseif value == TgtHeatCoolState.value.HeatOrCool then
                             mode = "auto"
                         end
-                        device:setProp("mode", mode)
+                        self:setProp("mode", mode)
                         return hap.Error.None
                     end),
-                    CoolThrholdTemp.new(iids.coolThrTemp, function (request, device)
-                        local value =  device:getProp("tar_temp")
-                        logger:info("Read CoolingThresholdTemperature: " .. value)
+                    CoolThrholdTemp.new(iids.coolThrTemp, function (request, self)
+                        local value =  self:getProp("tar_temp")
+                        self.logger:info("Read CoolingThresholdTemperature: " .. value)
                         return tonumber(value), hap.Error.None
-                    end, function (request, value, device)
-                        logger:info("Write CoolingThresholdTemperature: " .. value)
-                        device:setProp("tar_temp", math.tointeger(value))
+                    end, function (request, value, self)
+                        self.logger:info("Write CoolingThresholdTemperature: " .. value)
+                        self:setProp("tar_temp", math.tointeger(value))
                         return hap.Error.None
                     end, 16, 30, 1),
-                    HeatThrholdTemp.new(iids.heatThrTemp, function (request, device)
-                        local value = device:getProp("tar_temp")
-                        logger:info("Read HeatingThresholdTemperature: " .. value)
+                    HeatThrholdTemp.new(iids.heatThrTemp, function (request, self)
+                        local value = self:getProp("tar_temp")
+                        self.logger:info("Read HeatingThresholdTemperature: " .. value)
                         return tonumber(value), hap.Error.None
-                    end, function (request, value, device)
-                        logger:info("Write HeatingThresholdTemperature: " .. value)
-                        device:setProp("tar_temp", math.tointeger(value))
+                    end, function (request, value, self)
+                        self.logger:info("Write HeatingThresholdTemperature: " .. value)
+                        self:setProp("tar_temp", math.tointeger(value))
                         return hap.Error.None
                     end, 16, 30, 1),
-                    SwingMode.new(iids.swingMode, function (request, device)
+                    SwingMode.new(iids.swingMode, function (request, self)
                         local value
-                        if device:getProp("ver_swing") == "on" then
+                        if self:getProp("ver_swing") == "on" then
                             value = SwingMode.value.Enabled
                         else
                             value = SwingMode.value.Disabled
                         end
-                        logger:info("Read SwingMode: " .. util.searchKey(SwingMode.value, value))
+                        self.logger:info("Read SwingMode: " .. util.searchKey(SwingMode.value, value))
                         return value, hap.Error.None
-                    end, function (request, value, device)
-                        logger:info("Write SwingMode: " .. util.searchKey(SwingMode.value, value))
+                    end, function (request, value, self)
+                        self.logger:info("Write SwingMode: " .. util.searchKey(SwingMode.value, value))
                         local mode
                         if value == SwingMode.value.Enabled then
                             mode = "on"
                         else
                             mode = "off"
                         end
-                        device:setProp("ver_swing", mode)
+                        self:setProp("ver_swing", mode)
                         return hap.Error.None
                     end)
                 }
             }
         },
         cbs = {
-            identify = function (request, context)
-                logger:info("Identify callback is called.")
+            identify = function (request, self)
+                self.logger:info("Identify callback is called.")
                 return hap.Error.None
             end
         },
