aa67169546266427b3642ffd32b8269cf7c85f27
KNpTrue <zebin.wu@foxmail.com>
2021-09-14 22:29:05 +0800

    Rollback the method of setting object funcs.

--- plugins/miio/device.lua
+++ plugins/miio/device.lua
@@ -3,15 +3,6 @@ local timer = require "timer"
 
 local device = {}
 
----@class MiioDevice:table Device object.
----
----@field props table Properties table.
----@field cmdQue table Command queue.
----@field logger logger Logger object.
----@field pcb MiioPcb Miio PCB.
----@field timeout integer Request timeout.
-local _device = {}
-
 ---@alias DeviceState
 ---| '"NOINIT"'
 ---| '"IDLE"'
@@ -31,130 +22,6 @@ local _device = {}
 ---@field hw_ver string Hardware version.
 ---@field netif MiioDeviceNetIf Network interface.
 
-local function enque(que, ...)
-    local last = que.last
-    que[last] = {...}
-    que.last = last + 1
-end
-
-local function deque(que)
-    local first = que.first
-    local cmd = que[first]
-    que[first] = nil
-    que.first = first + 1
-    return table.unpack(cmd)
-end
-
----Dispatch the requests.
----@param self MiioDevice
-local function dispatch(self)
-    if self.state ~= "IDLE" then
-        return
-    end
-    local que = self.cmdQue
-    if que.first ~= que.last then
-        self.pcb:request(function (err, result, self, respCb, ...)
-            respCb(err, result, ...)
-            self.state = "IDLE"
-            dispatch(self)
-        end, self.timeout, deque(que))
-        self.state = "BUSY"
-    end
-end
-
-local function handleDoneCb(self, info)
-    self.done(self, info, table.unpack(self.args))
-    self.done = nil
-    self.args = nil
-end
-
----Handle sync proeprties.
----@param self MiioDevice
-local function handleSyncProps(self)
-    self.logger:debug("Syncing properties ...")
-    self:request(function (err, result, self)
-        local names = self.propNames
-        if result then
-            assert(#result == #names)
-            local props = self.props
-            for i, v in ipairs(result) do
-                local name = names[i]
-                if props[name] ~= v then
-                    props[name] = v
-                    self:update(name, table.unpack(self.updateArgs))
-                end
-            end
-        end
-        local ms = math.random(3000, 5000)
-        self.logger:debug(("Sync properties after %dms."):format(ms))
-        self.timer:start(ms, handleSyncProps, self)
-    end, "get_prop", self.propNames, self)
-end
-
----Start a request and ``respCb`` will be called when a response is received.
----@param respCb fun(err: MiioError|string|nil, result: any, ...) Response callback.
----@param method string The request method.
----@param params? table Array of parameters.
-function _device:request(respCb, method, params, ...)
-    assert(type(respCb) == "function")
-
-    enque(self.cmdQue, method, params, self, respCb, ...)
-    dispatch(self)
-end
-
----Start syncing properties.
-function _device:startSyncProps()
-    self:request(function (err, result, self)
-        local names = self.propNames
-        if result then
-            assert(#result == #names)
-            for i, v in ipairs(result) do
-                self.props[names[i]] = v
-            end
-        end
-        local ms = math.random(3000, 6000)
-        self.logger:debug(("Sync properties after %dms ..."):format(ms))
-        self.timer:start(ms, handleSyncProps, self)
-    end, "get_prop", self.propNames, self)
-end
-
----Register properties.
----@param names string[] Property names.
----@param update fun(self: MiioDevice, name: string, ...) The callback will be called when the property is updated.
-function _device:registerProps(names, update, ...)
-    assert(type(names) == "table")
-    assert(type(update) == "function")
-
-    self.update = update
-    self.propNames = names
-    self.updateArgs = {...}
-    self.timer = timer.create()
-
-    self:startSyncProps()
-end
-
----Get property.
----@param name string Property name.
----@return any value Property value.
-function _device:getProp(name)
-    return self.props[name]
-end
-
----Set property.
----@param name string Property name.
----@param value any Property value.
-function _device:setProp(name, value)
-    if self.props[name] == value then
-        return
-    end
-    self.props[name] = value
-    self:request(function (err, result, self, name)
-        if result then
-            self:update(name, table.unpack(self.updateArgs))
-        end
-    end, "set_" .. name, {value}, self, name)
-end
-
 ---Create a device object.
 ---@param done fun(self: MiioDevice, info: MiioDeviceInfo, ...) Callback will be called after the device is created.
 ---@param timeout integer Timeout period (in milliseconds).
@@ -168,15 +35,54 @@ function device.create(done, timeout, addr, token, ...)
     assert(type(token) == "string")
     assert(#token == 16)
 
+    ---@class MiioDevice:table Device object.
     local o = {
         logger = log.getLogger("miio.device:" .. addr),
         done = done,
         args = {...},
+        pcb = nil, ---@type MiioPcb
         timeout = timeout,
         state = "NOINIT",
         cmdQue = {first = 0, last = 0},
         props = {}
     }
+
+    local function enque(que, ...)
+        local last = que.last
+        que[last] = {...}
+        que.last = last + 1
+    end
+
+    local function deque(que)
+        local first = que.first
+        local cmd = que[first]
+        que[first] = nil
+        que.first = first + 1
+        return table.unpack(cmd)
+    end
+
+    ---Dispatch the requests.
+    ---@param self MiioDevice
+    local function dispatch(self)
+        if self.state ~= "IDLE" then
+            return
+        end
+        local que = self.cmdQue
+        if que.first ~= que.last then
+            self.pcb:request(function (err, result, self, respCb, ...)
+                respCb(err, result, ...)
+                self.state = "IDLE"
+                dispatch(self)
+            end, self.timeout, deque(que))
+            self.state = "BUSY"
+        end
+    end
+
+    local function handleDoneCb(self, info)
+        self.done(self, info, table.unpack(self.args))
+        self.done = nil
+        self.args = nil
+    end
 
     o.scanCtx = protocol.scan(function (addr, devid, stamp, self, token)
         self.scanTimer:cancel()
@@ -208,9 +114,83 @@ function device.create(done, timeout, addr, token, ...)
         handleDoneCb(self)
     end, o)
 
-    setmetatable(o, {
-        __index = _device
-    })
+    ---Start a request and ``respCb`` will be called when a response is received.
+    ---@param respCb fun(err: MiioError|string|nil, result: any, ...) Response callback.
+    ---@param method string The request method.
+    ---@param params? table Array of parameters.
+    function o:request(respCb, method, params, ...)
+        assert(type(respCb) == "function")
+
+        enque(self.cmdQue, method, params, self, respCb, ...)
+        dispatch(self)
+    end
+
+    local function syncPropsTimerCb(arg)
+        local self, names = table.unpack(arg)
+        self.logger:debug("Syncing properties ...")
+        self:request(function (err, result, self, names)
+            if result then
+                assert(#result == #names)
+                local props = self.props
+                for i, v in ipairs(result) do
+                    local name = names[i]
+                    if props[name] ~= v then
+                        props[name] = v
+                        self:update(name, table.unpack(self.updateArgs))
+                    end
+                end
+            end
+            local ms = math.random(3000, 5000)
+            self.logger:debug(("Sync properties after %dms."):format(ms))
+            self.timer:start(ms, syncPropsTimerCb, {self, names})
+        end, "get_prop", names, self, names)
+    end
+
+    ---Register properties.
+    ---@param names string[] Property names.
+    ---@param update fun(self: MiioDevice, name: string, ...) The callback will be called when the property is updated.
+    function o:registerProps(names, update, ...)
+        assert(type(names) == "table")
+        assert(type(update) == "function")
+
+        self.update = update
+        self.updateArgs = {...}
+        self.timer = timer.create()
+
+        self:request(function (err, result, self, names)
+            if result then
+                assert(#result == #names)
+                for i, v in ipairs(result) do
+                    self.props[names[i]] = v
+                end
+            end
+            local ms = math.random(3000, 5000)
+            self.logger:debug(("Sync properties after %dms ..."):format(ms))
+            self.timer:start(ms, syncPropsTimerCb, {self, names})
+        end, "get_prop", names, self, names)
+    end
+
+    ---Get property.
+    ---@param name string Property name.
+    ---@return any value Property value.
+    function o:getProp(name)
+        return self.props[name]
+    end
+
+    ---Set property.
+    ---@param name string Property name.
+    ---@param value any Property value.
+    function o:setProp(name, value)
+        if self.props[name] == value then
+            return
+        end
+        self.props[name] = value
+        self:request(function (err, result, self, name)
+            if result then
+                self:update(name, table.unpack(self.updateArgs))
+            end
+        end, "set_" .. name, {value}, self, name)
+    end
 
     return o
 end
