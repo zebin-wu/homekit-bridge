8bbc58f5b8a0c3285df5771445b3c53325dcd830
Zebin Wu <zebin.wu@foxmail.com>
2022-01-17 21:40:44 +0800

    Update module HTTP definition.

--- bridge/meta/http.lua
+++ bridge/meta/http.lua
@@ -10,16 +10,28 @@ local http = {}
 ---| '"PATCH"'
 ---| '"DELETE"'
 
+---@class HTTPClient HTTP client.
+local client = {}
+
+---Connect to HTTP server and return a client.
+---@param host string Server host name or IP address.
+---@param tls boolean Whether to enable TLS.
+---@param timeout? integer Timeout period (in milliseconds).
+---@return HTTPClient client HTTP client.
+---@nodiscard
+function http.connect(host, tls, timeout) end
+
 ---Start a HTTP request.
 ---@param method HTTPMethod The request method.
----@param url string The request URL.
+---@param path string The request path.
 ---@param headers table<string, string> The request headers.
 ---@param content? string The request content.
----@param timeout? integer Timeout period (in milliseconds).
 ---@return integer statuscode The response status code.
 ---@return table<string, string> headers The response headers.
 ---@return string content The response content.
 ---@nodiscard
-function http.request(method, url, headers, content, timeout) end
+function client:request(method, path, headers, content) end
+
+function client:close() end
 
 return http
