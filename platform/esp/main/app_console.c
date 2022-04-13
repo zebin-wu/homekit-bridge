da729b042ed389acc909aa1a8efbc751a28a838e
Zebin Wu <zebin.wu@foxmail.com>
2022-04-13 21:19:12 +0800

    ESP: Refactor app_console.

--- platform/esp/main/app_console.c
+++ platform/esp/main/app_console.c
@@ -10,9 +10,6 @@
 #include <esp_log.h>
 #include <esp_system.h>
 #include <esp_console.h>
-#include <esp_vfs_dev.h>
-#include <driver/uart.h>
-#include <linenoise/linenoise.h>
 #include <cmd_system.h>
 
 #include "app_console.h"
@@ -22,167 +19,27 @@
 
 #define PROMPT_STR CONFIG_IDF_TARGET
 
-typedef enum {
-    APP_CONSOLE_STATE_NOINIT,
-    APP_CONSOLE_STATE_INITED,
-    APP_CONSOLE_STATE_STARTED,
-} console_state;
-
-static const char *console_state_strs[] = {
-    [APP_CONSOLE_STATE_NOINIT] = "noinit",
-    [APP_CONSOLE_STATE_INITED] = "inited",
-    [APP_CONSOLE_STATE_STARTED] = "started",
-};
-
-static const char *TAG = "console";
-static console_state gv_state = APP_CONSOLE_STATE_NOINIT;
-
-static void app_console_task(void *arg)
-{
-    /* Prompt to be printed before each line.
-     * This can be customized, made dynamic, etc.
-     */
-    const char* prompt = LOG_COLOR_I PROMPT_STR "> " LOG_RESET_COLOR;
-
-    printf("\n"
-           "Type 'help' to get the list of commands.\n"
-           "Use UP/DOWN arrows to navigate through command history.\n"
-           "Press TAB when typing command name to auto-complete.\n");
-
-    /* Figure out if the terminal supports escape sequences */
-    int probe_status = linenoiseProbe();
-    if (probe_status) { /* zero indicates success */
-        printf("\n"
-               "Your terminal application does not support escape sequences.\n"
-               "Line editing and history features are disabled.\n"
-               "On Windows, try using Putty instead.\n");
-        linenoiseSetDumbMode(1);
-#if CONFIG_LOG_COLORS
-        /* Since the terminal doesn't support escape sequences,
-         * don't use color codes in the prompt.
-         */
-        prompt = PROMPT_STR "> ";
-#endif //CONFIG_LOG_COLORS
-    }
-    gv_state = APP_CONSOLE_STATE_STARTED;
-
-    /* Main loop */
-    while(true) {
-        /* Get a line using linenoise.
-         * The line is returned when ENTER is pressed.
-         */
-        char* line = linenoise(prompt);
-        if (line == NULL) { /* Ignore empty lines */
-            continue;
-        }
-
-        /* Try to run the command */
-        int ret;
-        esp_err_t err = esp_console_run(line, &ret);
-        if (err == ESP_ERR_NOT_FOUND) {
-            printf("Unrecognized command\n");
-        } else if (err == ESP_ERR_INVALID_ARG) {
-            // command was empty
-        } else if (err == ESP_OK && ret != ESP_OK) {
-            printf("Command returned non-zero error code: 0x%x (%s)\n", ret, esp_err_to_name(ret));
-        } else if (err != ESP_OK) {
-            printf("Internal error: %s\n", esp_err_to_name(err));
-        }
-        /* linenoise allocates line buffer on the heap, so need to free it */
-        linenoiseFree(line);
-    }
-    gv_state = APP_CONSOLE_STATE_INITED;
-}
+static esp_console_repl_t *grepl;
 
 void app_console_init(void)
 {
-    if (gv_state != APP_CONSOLE_STATE_NOINIT) {
-        ESP_LOGE(TAG, "%s: Invalid state. Current state: %s.", __func__, console_state_strs[gv_state]);
-        return;
-    }
+    assert(grepl == NULL);
+    esp_console_repl_config_t repl_config = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
+    esp_console_dev_uart_config_t uart_config = ESP_CONSOLE_DEV_UART_CONFIG_DEFAULT();
 
-    /* Drain stdout before reconfiguring it */
-    fflush(stdout);
-    fsync(fileno(stdout));
-
-    /* Disable buffering on stdin */
-    setvbuf(stdin, NULL, _IONBF, 0);
-
-#ifdef CONFIG_ESP_CONSOLE_USB_CDC
-    /* Minicom, screen, idf_monitor send CR when ENTER key is pressed */
-    esp_vfs_dev_cdcacm_set_rx_line_endings(ESP_LINE_ENDINGS_CR);
-    /* Move the caret to the beginning of the next line on '\n' */
-    esp_vfs_dev_cdcacm_set_tx_line_endings(ESP_LINE_ENDINGS_CRLF);
-
-    /* Enable non-blocking mode on stdin and stdout */
-    fcntl(fileno(stdout), F_SETFL, 0);
-    fcntl(fileno(stdin), F_SETFL, 0);
-#else // !CONFIG_ESP_CONSOLE_USB_CDC
-    /* Minicom, screen, idf_monitor send CR when ENTER key is pressed */
-    esp_vfs_dev_uart_port_set_rx_line_endings(CONFIG_ESP_CONSOLE_UART_NUM, ESP_LINE_ENDINGS_CR);
-    /* Move the caret to the beginning of the next line on '\n' */
-    esp_vfs_dev_uart_port_set_tx_line_endings(CONFIG_ESP_CONSOLE_UART_NUM, ESP_LINE_ENDINGS_CRLF);
-
-    /* Configure UART. Note that REF_TICK is used so that the baud rate remains
-     * correct while APB frequency is changing in light sleep mode.
-     */
-    const uart_config_t uart_config = {
-            .baud_rate = CONFIG_ESP_CONSOLE_UART_BAUDRATE,
-            .data_bits = UART_DATA_8_BITS,
-            .parity = UART_PARITY_DISABLE,
-            .stop_bits = UART_STOP_BITS_1,
-#if CONFIG_IDF_TARGET_ESP32 || CONFIG_IDF_TARGET_ESP32S2
-        .source_clk = UART_SCLK_REF_TICK,
-#else
-        .source_clk = UART_SCLK_XTAL,
-#endif
-    };
-    /* Install UART driver for interrupt-driven reads and writes */
-    ESP_ERROR_CHECK( uart_driver_install(CONFIG_ESP_CONSOLE_UART_NUM,
-            256, 0, 0, NULL, 0) );
-    ESP_ERROR_CHECK( uart_param_config(CONFIG_ESP_CONSOLE_UART_NUM, &uart_config) );
-
-    /* Tell VFS to use UART driver */
-    esp_vfs_dev_uart_use_driver(CONFIG_ESP_CONSOLE_UART_NUM);
-#endif // CONFIG_ESP_CONSOLE_USB_CDC
-
-    /* Initialize the console */
-    esp_console_config_t console_config = {
-            .max_cmdline_args = 8,
-            .max_cmdline_length = 256,
-#if CONFIG_LOG_COLORS
-            .hint_color = atoi(LOG_COLOR_CYAN)
-#endif
-    };
-    ESP_ERROR_CHECK( esp_console_init(&console_config) );
-
-    /* Configure linenoise line completion library */
-    /* Enable multiline editing. If not set, long commands will scroll within
-     * single line.
-     */
-    linenoiseSetMultiLine(1);
-
-    /* Tell linenoise where to get command completions and hints */
-    linenoiseSetCompletionCallback(&esp_console_get_completion);
-    linenoiseSetHintsCallback((linenoiseHintsCallback*) &esp_console_get_hint);
-
-    /* Don't return empty lines */
-    linenoiseAllowEmpty(false);
+    repl_config.prompt = PROMPT_STR ">";
+    repl_config.max_cmdline_length = 256;
+    repl_config.task_stack_size = APP_CONSOLE_TASK_STACKSIZE;
 
     /* Register commands */
     esp_console_register_help_command();
     register_system();
 
-    gv_state = APP_CONSOLE_STATE_INITED;
+    ESP_ERROR_CHECK(esp_console_new_repl_uart(&uart_config, &repl_config, &grepl));
 }
 
 void app_console_start(void)
 {
-    if (gv_state != APP_CONSOLE_STATE_INITED) {
-        ESP_LOGE(TAG, "%s: Invalid state. Current state: %s.", __func__, console_state_strs[gv_state]);
-        return;
-    }
-
-    xTaskCreate(app_console_task, "console", APP_CONSOLE_TASK_STACKSIZE,
-        NULL, APP_CONSOLE_TASK_PRIORITY, NULL);
+    assert(grepl);
+    ESP_ERROR_CHECK(esp_console_start_repl(grepl));
 }
