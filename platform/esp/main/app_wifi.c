ad76dc270344567923e6454b732eaef62a10e3d8
Zebin Wu <zebin.wu@foxmail.com>
2023-06-08 22:30:58 +0800

    esp32: add wifi scan command.

--- platform/esp/main/app_wifi.c
+++ platform/esp/main/app_wifi.c
@@ -5,9 +5,12 @@
 // See [CONTRIBUTORS.md] for the list of homekit-bridge project authors.
 
 #include <string.h>
+#include <stdio.h>
 #include <esp_system.h>
 #include <esp_console.h>
 #include <argtable3/argtable3.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/event_groups.h>
 #include <esp_wifi.h>
 #include <esp_event.h>
 #include <esp_log.h>
@@ -45,6 +48,22 @@ static const char *TAG = "app_wifi";
 
 static app_wifi_desc gv_wifi_desc;
 
+static const char *auth_mode_strs[] = {
+    [WIFI_AUTH_OPEN] = "Open",
+    [WIFI_AUTH_WEP] = "WEP",
+    [WIFI_AUTH_WPA_PSK] = "WPA",
+    [WIFI_AUTH_WPA2_PSK] = "WPA2",
+    [WIFI_AUTH_WPA_WPA2_PSK] = "WPA/WPA2",
+    [WIFI_AUTH_WPA2_ENTERPRISE] = "WPA2-Enterprise",
+    [WIFI_AUTH_WPA3_PSK] = "WPA3",
+    [WIFI_AUTH_WPA2_WPA3_PSK] = "WPA2/WPA3",
+    [WIFI_AUTH_WAPI_PSK] = "WAPI",
+    [WIFI_AUTH_OWE] = "OWE",
+};
+
+static EventGroupHandle_t app_wifi_event_group;
+const int SCAN_DONE_BIT = BIT0;
+
 static void event_handler(void* arg, esp_event_base_t event_base,
                                 int32_t event_id, void* event_data) {
     static int retry = 0;
@@ -79,6 +98,38 @@ static void event_handler(void* arg, esp_event_base_t event_base,
             ssid[evt->ssid_len] = '\0';
             ESP_LOGI(TAG, "Connected to AP \"%s\", channel: %u", ssid, evt->channel);
             ESP_ERROR_CHECK(esp_netif_create_ip6_linklocal((esp_netif_t *)arg));
+            break;
+        }
+        case WIFI_EVENT_SCAN_DONE:
+        {
+            uint16_t sta_number = 0;
+            uint8_t i;
+            wifi_ap_record_t *ap_list_buffer;
+
+            esp_wifi_scan_get_ap_num(&sta_number);
+            if (!sta_number) {
+                printf("No AP found\n");
+                xEventGroupSetBits(app_wifi_event_group, SCAN_DONE_BIT);
+                return;
+            }
+
+            ap_list_buffer = malloc(sta_number * sizeof(wifi_ap_record_t));
+            if (ap_list_buffer == NULL) {
+                xEventGroupClearBits(app_wifi_event_group, SCAN_DONE_BIT);
+                return;
+            }
+
+            if (esp_wifi_scan_get_ap_records(&sta_number, (wifi_ap_record_t *)ap_list_buffer) == ESP_OK) {
+                printf("%-32s\t%-16s\t%s\n", "SSID", "Auth Mode", "RSSI");
+                for (i = 0; i < sta_number; i++) {
+                    printf("%-32s\t%-16s\t%d\n",
+                        ap_list_buffer[i].ssid,
+                        auth_mode_strs[ap_list_buffer[i].authmode],
+                        ap_list_buffer[i].rssi);
+                }
+            }
+            xEventGroupSetBits(app_wifi_event_group, SCAN_DONE_BIT);
+            free(ap_list_buffer);
             break;
         }
         default:
@@ -177,12 +228,34 @@ void app_wifi_connect(const char *ssid, const char *password)
     ESP_ERROR_CHECK(esp_wifi_connect());
 }
 
+static int wifi_cmd_sta_scan(const char *ssid)
+{
+    wifi_scan_config_t scan_config = {0};
+    scan_config.ssid = (uint8_t *)ssid;
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    if (esp_wifi_scan_start(&scan_config, false) != ESP_OK) {
+        return 1;
+    }
+
+    app_wifi_event_group = xEventGroupCreate();
+    int bits = xEventGroupWaitBits(app_wifi_event_group, SCAN_DONE_BIT,
+                                   pdFALSE, pdTRUE, portMAX_DELAY);
+    return (bits & SCAN_DONE_BIT) == 0;
+}
+
 /** Arguments used by 'join' function */
 static struct {
     struct arg_str *ssid;
     struct arg_str *password;
     struct arg_end *end;
 } join_args;
+
+/** Arguments used by 'scan' function */
+static struct {
+    struct arg_str *ssid;
+    struct arg_end *end;
+} scan_args;
 
 static int app_wifi_join_cmd(int argc, char **argv)
 {
@@ -194,6 +267,18 @@ static int app_wifi_join_cmd(int argc, char **argv)
     app_wifi_connect(join_args.ssid->sval[0], 
         join_args.password->sval[0]);
     return 0;
+}
+
+static int app_wifi_scan_cmd(int argc, char **argv)
+{
+    int nerrors = arg_parse(argc, argv, (void **) &scan_args);
+
+    if (nerrors != 0) {
+        arg_print_errors(stderr, scan_args.end, argv[0]);
+        return 1;
+    }
+
+    return wifi_cmd_sta_scan(scan_args.ssid->count == 1 ? scan_args.ssid->sval[0] : NULL);
 }
 
 void app_wifi_register_cmd(void)
@@ -211,6 +296,19 @@ void app_wifi_register_cmd(void)
     };
 
     ESP_ERROR_CHECK(esp_console_cmd_register(&join_cmd));
+
+    scan_args.ssid = arg_str0(NULL, NULL, "<ssid>", "SSID of AP want to be scanned");
+    scan_args.end = arg_end(1);
+
+    const esp_console_cmd_t scan_cmd = {
+        .command = "scan",
+        .help = "WiFi is station mode, start scan ap",
+        .hint = NULL,
+        .func = &app_wifi_scan_cmd,
+        .argtable = &scan_args,
+    };
+
+    ESP_ERROR_CHECK(esp_console_cmd_register(&scan_cmd));
 }
 
 void app_wifi_set_connected_cb(void (*cb)(void)) {
